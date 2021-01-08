31fcac5fbe7e1be4da1528f454e0c931c08d0400
ripee <ripee.dev@gmail.com>
2021-01-08 23:59:02 +0100

    gts210ltexx: RR-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 The rrOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gts210ltexx.mk
+    $(LOCAL_DIR)/rr_gts210ltexx.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gts210ltexx-user \
-    lineage_gts210ltexx-userdebug \
-    lineage_gts210ltexx-eng \
+    rr_gts210ltexx-user \
+    rr_gts210ltexx-userdebug \
+    rr_gts210ltexx-eng \
